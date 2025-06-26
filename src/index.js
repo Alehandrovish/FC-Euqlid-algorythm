47b62bce4c0dc19eb22809b9370e8886475c1298
Alex <sascha.rumin@gmail.com>
2025-06-26 13:25:57 +0300

    Add new verification and put call of function in try catch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 "use strict";
 
 function euqlid(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("a or b is not a number");
+  }
   if (a < 0 || b < 0) throw new Error("a or b below zero");
-  const smollest = a < b ? a : b;
-  const biggest = smollest !== a ? a : b;
-  const rest = biggest % smollest;
-  return rest === 0 ? smollest : euqlid(rest, smollest);
+  const smallest = a < b ? a : b;
+  const biggest = smallest !== a ? a : b;
+  const rest = biggest % smallest;
+  return rest === 0 ? smallest : euqlid(rest, smallest);
 }
-console.log(`NOD: ${euqlid(48, 18)}`); //48, 18 = 6; 56, 98 = 14; 105, 252 = 21, -5, 6 = error; 5, -6 = error;
+try {
+  console.log(`NOD: ${euqlid(48, 18)}`); //48, 18 = 6; 56, 98 = 14; 105, 252 = 21, -5, 6 = error; 5, -6 = error;
+} catch (error) {
+  console.error("Error:", error.message);
+}
