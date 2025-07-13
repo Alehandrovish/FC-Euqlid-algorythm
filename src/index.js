7654439c977033e6d1d07b0f82a1c42c3f5ccbd3
Alex <sascha.rumin@gmail.com>
2025-07-13 15:48:28 +0300

    Fixing try..catch

--- src/index.js
+++ src/index.js
@@ -1,43 +1,25 @@
 "use strict";
 
 function euqlid(a, b) {
-  if (
-    typeof a !== "number" ||
-    typeof b !== "number" ||
-    !Number.isFinite(a) ||
-    !Number.isFinite(b)
-  ) {
-    throw new Error("a and b must be finite numbers");
-  }
-  if (a <= 0 || b <= 0) throw new Error("a or b below or equal zero");
-  if (a === b) return a; // as NOD
-  let smallest = a < b ? a : b;
-  let biggest = smallest === b ? a : b;
-  let rest;
-  while (smallest !== 0) {
-    rest = biggest % smallest;
-    biggest = smallest;
-    smallest = rest;
-  }
-
-  return biggest;
-}
-
-const testCases = [
-  [48, 18],
-  [56, 98],
-  [105, 252],
-  [-5, 6],
-  ["a", 18],
-  [0, 5],
-  [Infinity, 3],
-  [5, NaN],
-];
-
-for (const [a, b] of testCases) {
   try {
-    console.log(`NOD(${a}, ${b}) = ${euqlid(a, b)}`);
-  } catch (error) {
-    console.error(`Помилка для (${a}, ${b}): ${error.message}`);
+    if (
+      typeof a !== "number" ||
+      typeof b !== "number" ||
+      !Number.isFinite(a) ||
+      !Number.isFinite(b)
+    ) {
+      throw new Error("a and b must be finite numbers");
+    }
+    if (a <= 0 || b <= 0) throw new Error("a or b below or equal zero");
+    if (a === b) return a; // as NOD
+    if (a < b) {
+      [b, a] = [a, b];
+    }
+    const rest = a % b;
+    return rest === 0 ? b : euqlid(rest, b);
+  } catch (err) {
+    return err.message;
   }
 }
+
+console.log(euqlid(18, 48)); //48, 18 = 6, 56, 98 = 14, 105, 252 = 21, -5, 6 = error, "a", 18 = error, 0, 5 = error, Infinity, 3 = error, 5, NaN = error
